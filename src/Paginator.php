e5c0819227cac42df1341be4dfe893afd43eec7a
Čamaj Vladimír <vladimir.camaj@gmail.com>
2021-07-08 15:25:01 +0200

    Git init

---
+++ src/Paginator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Camohub;
+
+
+use Illuminate\Support\Facades\Log;
+
+
+class Paginator
+{
+
+	public function __construct()
+	{
+		Log::debug('*******************************************************');
+		Log::debug(' This is Camohub\Paginator test.');
+		Log::debug('*******************************************************');
+	}
+}
---
+++ src/PaginatorServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Camohub\Paginator;
+
+
+use Illuminate\Support\ServiceProvider;
+
+
+/**
+ * Class PasswordServiceProvider
+ */
+class PaginatorServiceProvider extends ServiceProvider
+{
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app->bind('paginator', Paginator::class);
+	}
+}
