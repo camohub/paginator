2f16e8a26d75a42d78ebfda3e158aa64e553596e
Čamaj Vladimír <vladimir.camaj@gmail.com>
2021-07-09 10:12:04 +0200

    base.blade.php - styles

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,12 +4,12 @@
 	<ul class="pagination">
 		@if( $currentPage === 1 )
 			<li class="page-item disabled">
-				<span class="page-link">«</span>
+				<span class="page-link">&lsaquo;</span>
 			</li>
 		@else
 			<?php $params = array_merge($routeParams, [$pageParam => max(1, $currentPage - 1)]); ?>
 			<li class="page-item">
-				<a href="{{route($routeName, $params)}}" class="page-link">«</a>
+				<a href="{{route($routeName, $params)}}" class="page-link">&lsaquo;</a>
 			</li>
 		@endif
 
@@ -33,11 +33,11 @@
 
 		@if( $currentPage === $lastPage )
 			<li class="page-item disabled"></li>
-			<span class="page-link">»</span>
+			<span class="page-link">&rsaquo;</span>
 		@else
 			<?php $params = array_merge($routeParams, [$pageParam => $step]); ?>
 			<li class="page-item">
-				<a class="page-link" href="{{route($routeName, $params)}}">»</a>
+				<a class="page-link" href="{{route($routeName, $params)}}">&rsaquo;</a>
 			</li>
 		@endif
 	</ul>
